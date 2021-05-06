83224b154066e247ce198dc574e4ed54b7a359f1
davidventasmarin <dvm@medlinetec.eu>
2021-05-06 13:16:21 +0200

    Añadimos la condición para que cumpla los requisitos del nuevo test sin que fallen los otros.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizz(num):
-    if num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
         return "Fizz"
     elif num % 5 == 0:
         return "Buzz"
