dd1828760b554d6ef8b6b26b6c20b365187894ce
davidventasmarin <dvm@medlinetec.eu>
2021-05-11 11:12:15 +0200

    Test pasados, con un poco de dificultad

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,7 +5,7 @@ def test_divisible3():
     assert fizz(3)  == "Fizz"
     assert fizz(6)  == "Fizz"  
     assert fizz(9)  == "Fizz"
-    assert fizz(32) == "Fizz" 
+    #assert fizz(32) == "Fizz" 
 
 def test_divisible5():
     assert fizz(5)  == "Buzz"
