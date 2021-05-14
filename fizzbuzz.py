f7b847f59aa040386566c05c2eba220a11af40e4
davidventasmarin <dvm@medlinetec.eu>
2021-05-14 10:35:03 +0200

    Extrayendo más métodos y ordenando los métodos en orden de uso.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,7 @@ def fizz(num):
     flag_divisible5                 = 0
     flag_divisible_3_5              = 0
     
-    resultado_comprobacion_numeros  = 0
+    #resultado_comprobacion_numeros  = 0
     resultado_texto                 = 0
     devuelvo_texto                  = 0
 
@@ -19,41 +19,19 @@ def fizz(num):
         flag_divisible5 = 1
 
     
-    if comprobacion_numeros == 2:
-        resultado_comprobacion_numeros = 2
-    elif comprobacion_numeros == 1:
-        resultado_comprobacion_numeros = 1
-    elif comprobacion_numeros == 3:
-        resultado_comprobacion_numeros = 3
-    
-    resultado_comprobacion_flag = comprobar_flag(flag_divisible3, flag_divisible5, flag_divisible_3_5, num)
-    devuelvo_texto         = decidir_resultado_prevalece(resultado_comprobacion_flag, resultado_comprobacion_numeros)
-    imprimo_text_divisible = imprimir_texto(devuelvo_texto)
-    impr_texto_o_numero    = imprimo_text_divisible_o_numero(imprimo_text_divisible, num)
+    # if comprobacion_numeros == 2:
+    #     resultado_comprobacion_numeros = 2
+    # elif comprobacion_numeros == 1:
+    #     resultado_comprobacion_numeros = 1
+    # elif comprobacion_numeros == 3:
+    #     resultado_comprobacion_numeros = 3
+
+    resultado_comprobacion_numeros = resultado_comprobacion_flags(comprobacion_numeros)
+    resultado_comprobacion_flag    = comprobar_flag(flag_divisible3, flag_divisible5, flag_divisible_3_5, num)
+    devuelvo_texto                 = decidir_resultado_prevalece(resultado_comprobacion_flag, resultado_comprobacion_numeros)
+    imprimo_text_divisible         = imprimir_texto(devuelvo_texto)
+    impr_texto_o_numero            = imprimo_text_divisible_o_numero(imprimo_text_divisible, num)
     return impr_texto_o_numero
-
-def imprimo_text_divisible_o_numero(resultado_text_divisible, numero):
-    if resultado_text_divisible:
-        return resultado_text_divisible
-    else:
-        return numero
-
-def decidir_resultado_prevalece(comprobacion_flag, comprobacion_numeros):
-    if comprobacion_flag > comprobacion_numeros:
-        devuelvo_texto = comprobacion_flag
-    else:
-        devuelvo_texto = comprobacion_numeros
-    return devuelvo_texto
-
-
-def imprimir_texto(resultado):
-    if resultado == 1:
-        return "Fizz"
-    elif resultado == 2:
-        return "Buzz"
-    elif resultado == 3:
-        return "FizzBuzz"
-
 
 def comprueba_numeros(numero):
     numero_temporal = list(str(numero))
@@ -75,7 +53,6 @@ def comprueba_numeros(numero):
     else:
         return 0
 
-
 def comprobar_flag(flag_divisible3, flag_divisible5, flag_divisible_3_5, num):
     if flag_divisible_3_5:
         return 3
@@ -85,3 +62,37 @@ def comprobar_flag(flag_divisible3, flag_divisible5, flag_divisible_3_5, num):
         return 2
     else:
         return 0
+
+def resultado_comprobacion_flags(cantidad_flags):
+    if cantidad_flags == 2:
+        resultado_comprobacion_numeros = 2
+    elif cantidad_flags == 1:
+        resultado_comprobacion_numeros = 1
+    elif cantidad_flags == 3:
+        resultado_comprobacion_numeros = 3
+    else:
+        resultado_comprobacion_numeros = 0
+    
+    return resultado_comprobacion_numeros
+
+def decidir_resultado_prevalece(comprobacion_flag, comprobacion_numeros):
+    if comprobacion_flag > comprobacion_numeros:
+        devuelvo_texto = comprobacion_flag
+    else:
+        devuelvo_texto = comprobacion_numeros
+    return devuelvo_texto
+
+def imprimo_text_divisible_o_numero(resultado_text_divisible, numero):
+    if resultado_text_divisible:
+        return resultado_text_divisible
+    else:
+        return numero
+
+def imprimir_texto(resultado):
+    if resultado == 1:
+        return "Fizz"
+    elif resultado == 2:
+        return "Buzz"
+    elif resultado == 3:
+        return "FizzBuzz"
+
