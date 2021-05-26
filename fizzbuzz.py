6a31d6f4db98346e0a4eff04a00a6fd759a9d60d
davidventasmarin <dvm@medlinetec.eu>
2021-05-26 09:51:46 +0200

    Refactorizando el método comprueba número

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,7 +10,7 @@ def fizz(num):
 
     numero_temporal = list(str(num))
     
-    comprobacion_numeros = comprueba_numeros(num)
+    comprobacion_numeros = comprueba_numeros(numero_temporal)
     if num % 3 == 0 and num % 5 == 0:
         flag_divisible_3_5 = 1
     elif num % 3 == 0:
@@ -26,25 +26,19 @@ def fizz(num):
     return impr_texto_o_numero
 
 def comprueba_numeros(numero):
-    numero_temporal = list(str(numero))
-    
-    flag_contiene_3_5  = 0
-    
+    if 3 or 5 in numero:
+        que_numero_contiene = verifica_numeros_contenido(numero)
+        return que_numero_contiene
 
-    for n in numero_temporal:
-        if n == '3':
-            flag_contiene_3_5 += 1
-        if n == '5':
-            flag_contiene_3_5 += 2
+def verifica_numeros_contenido(numero):
+    flag_numero_contenido = 0
 
-    if flag_contiene_3_5 == 3:
-        return 3
-    elif flag_contiene_3_5 == 1:
-        return 1
-    elif flag_contiene_3_5 == 2:
-        return 2
-    else:
-        return 0
+    if '3' in numero:
+        flag_numero_contenido += 1
+    if '5' in numero:
+        flag_numero_contenido += 2
+
+    return flag_numero_contenido 
 
 def comprobar_flag(flag_divisible3, flag_divisible5, flag_divisible_3_5):
     if flag_divisible_3_5:
