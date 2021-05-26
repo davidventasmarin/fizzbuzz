72b47b85360d754df2d3ff0e5da521c21b5bf8af
davidventasmarin <dvm@medlinetec.eu>
2021-05-26 12:52:54 +0200

    Hemos pasado pylint al código, y nos ha dado un 5 de 10, ahora mismo lo tenemos en 9.80 de 10

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,33 +1,43 @@
+''' Vamos a crear una kata para entrenar habilidades de desarrollo '''
+
 def fizz(num):
+    ''' Imprimir fizz si tiene o es divisible entre 3
+        Imprimir Buzz si tiene o es divisible entre 5
+        Imprimir fizzbuss si se dan las dos condiciones anteriores
+        Imprimir el número si no se dan ninguna de las dos condiciones '''
     numero_temporal           = list(str(num))
     flag_divisible3           = divisible_3(num)
     flag_divisible5           = divisible_5(num)
-    
     comprobacion_numeros      = contiene_3_yo_5(numero_temporal)
-    comprobacion_is_divisible = comprobar_divisibilidad(flag_divisible3, flag_divisible5)
-    devuelvo_texto            = decidir_resultado_prevalece(comprobacion_is_divisible, comprobacion_numeros)
+    is_divisible              = comprobar_divisibilidad(flag_divisible3, flag_divisible5)
+    devuelvo_texto            = decidir_resultado_prevalece(is_divisible, comprobacion_numeros)
     imprimo_text_divisible    = imprimir_texto(devuelvo_texto)
     impr_texto_o_numero       = imprimir_texto_si_no_es_divisible(imprimo_text_divisible, num)
-    
     return impr_texto_o_numero
 
 def divisible_3(num):
+    ''' Ver si es divisible entre 3 '''
     if num % 3 == 0:
         return 1
+    return None
 
 def divisible_5(num):
+    ''' Ver si es divisible entre 5 '''
     if num % 5 == 0:
         return 1
+    return None
 
 def contiene_3_yo_5(numero):
+    ''' Verificamos en el número si contiene 3 y/o 5'''
     flag_numero_contenido = 0
     if '3' in numero:
         flag_numero_contenido += 1
     if '5' in numero:
         flag_numero_contenido += 2
-    return flag_numero_contenido 
+    return flag_numero_contenido
 
 def comprobar_divisibilidad(is_divisible3, is_divisible5):
+    ''' Debemos de mirar por que número es divisible '''
     devuelve_divisibilidad = 0
     if is_divisible3:
         devuelve_divisibilidad += 1
@@ -36,6 +46,7 @@ def comprobar_divisibilidad(is_divisible3, is_divisible5):
     return devuelve_divisibilidad
 
 def decidir_resultado_prevalece(comprobacion_is_divisible, comprobacion_numeros):
+    ''' Tenemos que ver si contiene 3 y 5 ya da igual que comprobemos la divisibilidad '''
     if comprobacion_is_divisible > comprobacion_numeros:
         devuelvo_texto = comprobacion_is_divisible
     else:
@@ -43,16 +54,17 @@ def decidir_resultado_prevalece(comprobacion_is_divisible, comprobacion_numeros)
     return devuelvo_texto
 
 def imprimir_texto_si_no_es_divisible(resultado_text_divisible, numero):
+    ''' Comprobar si el resultado es divisible, sino, imprimir el número '''
     if resultado_text_divisible:
         return resultado_text_divisible
-    else:
-        return numero
+    return numero
 
 def imprimir_texto(resultado):
+    ''' Aquí sabemos por las flags por que número es divisible, e imprimimos el texto '''
     if resultado == 1:
         return "Fizz"
     elif resultado == 2:
         return "Buzz"
     elif resultado == 3:
         return "FizzBuzz"
-
+    return None
